7d083c672196e3a1f321e4329895bb6ae60ca939
etienne-napoleone <etienne.napoleone@gmail.com>
2022-03-22 21:27:53 +0100

    feat: add colors and bugfixes

--- src/console.rs
+++ src/console.rs
@@ -1,7 +1,25 @@
 use std::io::{stdout, Write};
 
+#[derive(Clone)]
+pub enum Color {
+    Ignore,
+    Reset,
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
 pub fn flush() {
     stdout().flush().unwrap();
+}
+
+pub fn delete_line() {
+    print!("\x1b[2K")
 }
 
 pub fn hide_cursor() {
@@ -10,4 +28,27 @@ pub fn hide_cursor() {
 
 pub fn show_cursor() {
     print!("\x1b[?25h")
+}
+
+pub fn new_line() {
+    println!()
+}
+
+pub fn color(color: &Color) -> Option<String> {
+    match color {
+        Color::Ignore => None,
+        Color::Reset => Some(ansi_color(0)),
+        Color::Black => Some(ansi_color(30)),
+        Color::Red => Some(ansi_color(31)),
+        Color::Green => Some(ansi_color(32)),
+        Color::Yellow => Some(ansi_color(33)),
+        Color::Blue => Some(ansi_color(34)),
+        Color::Magenta => Some(ansi_color(35)),
+        Color::Cyan => Some(ansi_color(36)),
+        Color::White => Some(ansi_color(37)),
+    }
+}
+
+fn ansi_color(code: u64) -> String {
+    format!("\x1b[{}m", code)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
+pub use console::Color;
 pub use spinner::Spinach;
-pub use spinner::SpinnerData;
+pub use spinner::Spinner;
 
 mod console;
 mod spinner;
