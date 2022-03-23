e1bfdefeb2caa119b3261f7d05795b8e2fa5b7ae
etienne-napoleone <etienne.napoleone@gmail.com>
2022-03-23 08:57:50 +0100

    refactor: bring back spinach in lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,105 @@
-pub use spinner::Spinach;
+use std::sync::mpsc::{channel, Sender, TryRecvError};
+use std::thread;
+use std::time::Duration;
+
+use spinner::{Context, SpinnerCommand};
+
 pub use spinner::Spinner;
 pub use term::Color;
 
 mod spinner;
 mod term;
+
+pub struct Spinach {
+    sender: Sender<SpinnerCommand>,
+    handle: thread::JoinHandle<()>,
+}
+
+impl Spinach {
+    pub fn new(text: &'static str) -> Self {
+        let spinner = Spinner::default();
+        Self::run(spinner, text, term::Color::Cyan)
+    }
+
+    pub fn with_spinner(spinner: Spinner, text: &'static str) -> Self {
+        Self::run(spinner, text, term::Color::Cyan)
+    }
+
+    pub fn stop(
+        self,
+        symbol: Option<&'static str>,
+        text: Option<&'static str>,
+        color: Option<term::Color>,
+    ) {
+        self.sender
+            .send(SpinnerCommand::Stop {
+                symbol,
+                text,
+                color,
+            })
+            .expect("Could not stop spinner.");
+        self.handle.join().unwrap();
+    }
+
+    pub fn succeed(self, text: &'static str) {
+        self.stop(Some("✔"), Some(text), Some(term::Color::Green));
+    }
+
+    pub fn fail(self, text: &'static str) {
+        self.stop(Some("✖"), Some(text), Some(term::Color::Red));
+    }
+
+    pub fn warn(self, text: &'static str) {
+        self.stop(Some("⚠"), Some(text), Some(term::Color::Yellow));
+    }
+
+    pub fn info(self, text: &'static str) {
+        self.stop(Some("ℹ"), Some(text), Some(term::Color::Blue));
+    }
+
+    pub fn text(&self, text: &'static str) {
+        self.sender
+            .send(SpinnerCommand::Update { text })
+            .expect("Could not update spinner.");
+    }
+
+    fn run(config: Spinner, text: &'static str, color: term::Color) -> Self {
+        term::hide_cursor();
+
+        let (sender, receiver) = channel::<SpinnerCommand>();
+
+        let mut context = Context {
+            spinner: config,
+            text,
+            color,
+        };
+
+        let handle = thread::spawn(move || loop {
+            match receiver.try_recv() {
+                Ok(SpinnerCommand::Update { text }) => context.text = text,
+                Ok(SpinnerCommand::Stop {
+                    symbol,
+                    text,
+                    color,
+                }) => {
+                    context.render_with_override(symbol, text, color);
+                    term::new_line();
+                    term::show_cursor();
+                    break;
+                }
+                Err(TryRecvError::Disconnected) => {
+                    context.render();
+                    term::new_line();
+                    term::show_cursor();
+                    break;
+                }
+                _ => (),
+            }
+
+            context.render();
+            thread::sleep(Duration::from_millis(context.spinner.interval));
+        });
+
+        Self { sender, handle }
+    }
+}
--- src/spinner.rs
+++ src/spinner.rs
@@ -1,8 +1,3 @@
-use std::sync::mpsc::TryRecvError;
-use std::sync::mpsc::{channel, Sender};
-use std::thread;
-use std::time::Duration;
-
 use crate::term;
 
 pub struct Spinner {
@@ -43,10 +38,10 @@ impl Spinner {
     }
 }
 
-struct Context {
-    spinner: Spinner,
-    text: &'static str,
-    color: term::Color,
+pub struct Context {
+    pub spinner: Spinner,
+    pub text: &'static str,
+    pub color: term::Color,
 }
 
 impl Default for Context {
@@ -63,7 +58,7 @@ impl Default for Context {
 }
 
 impl Context {
-    fn render(&mut self) {
+    pub fn render(&mut self) {
         Self::print(
             self.spinner.next().unwrap_or_default(),
             self.text,
@@ -71,7 +66,7 @@ impl Context {
         );
     }
 
-    fn render_with_override(
+    pub fn render_with_override(
         &mut self,
         spinner_frame: Option<&str>,
         text: Option<&str>,
@@ -84,7 +79,7 @@ impl Context {
         Self::print(spinner_frame, text, &color);
     }
 
-    fn print(spinner_frame: &str, text: &str, color: &term::Color) {
+    pub fn print(spinner_frame: &str, text: &str, color: &term::Color) {
         term::delete_line();
         print!(
             "\r{}{}{} {}",
@@ -97,7 +92,7 @@ impl Context {
     }
 }
 
-enum SpinnerCommand {
+pub enum SpinnerCommand {
     Update {
         text: &'static str,
     },
@@ -106,98 +101,4 @@ enum SpinnerCommand {
         text: Option<&'static str>,
         color: Option<term::Color>,
     },
-}
-
-pub struct Spinach {
-    sender: Sender<SpinnerCommand>,
-    handle: thread::JoinHandle<()>,
-}
-
-impl Spinach {
-    pub fn new(text: &'static str) -> Self {
-        let spinner = Spinner::default();
-        Self::run(spinner, text, term::Color::Cyan)
-    }
-
-    pub fn with_spinner(spinner: Spinner, text: &'static str) -> Self {
-        Self::run(spinner, text, term::Color::Cyan)
-    }
-
-    pub fn stop(
-        self,
-        symbol: Option<&'static str>,
-        text: Option<&'static str>,
-        color: Option<term::Color>,
-    ) {
-        self.sender
-            .send(SpinnerCommand::Stop {
-                symbol,
-                text,
-                color,
-            })
-            .expect("Could not stop spinner.");
-        self.handle.join().unwrap();
-    }
-
-    pub fn succeed(self, text: &'static str) {
-        self.stop(Some("✔"), Some(text), Some(term::Color::Green));
-    }
-
-    pub fn fail(self, text: &'static str) {
-        self.stop(Some("✖"), Some(text), Some(term::Color::Red));
-    }
-
-    pub fn warn(self, text: &'static str) {
-        self.stop(Some("⚠"), Some(text), Some(term::Color::Yellow));
-    }
-
-    pub fn info(self, text: &'static str) {
-        self.stop(Some("ℹ"), Some(text), Some(term::Color::Blue));
-    }
-
-    pub fn text(&self, text: &'static str) {
-        self.sender
-            .send(SpinnerCommand::Update { text })
-            .expect("Could not update spinner.");
-    }
-
-    fn run(config: Spinner, text: &'static str, color: term::Color) -> Self {
-        term::hide_cursor();
-
-        let (sender, receiver) = channel::<SpinnerCommand>();
-
-        let mut context = Context {
-            spinner: config,
-            text,
-            color,
-        };
-
-        let handle = thread::spawn(move || loop {
-            match receiver.try_recv() {
-                Ok(SpinnerCommand::Update { text }) => context.text = text,
-                Ok(SpinnerCommand::Stop {
-                    symbol,
-                    text,
-                    color,
-                }) => {
-                    context.render_with_override(symbol, text, color);
-                    term::new_line();
-                    term::show_cursor();
-                    break;
-                }
-                Err(TryRecvError::Disconnected) => {
-                    context.render();
-                    term::new_line();
-                    term::show_cursor();
-                    break;
-                }
-                _ => (),
-            }
-
-            context.render();
-            thread::sleep(Duration::from_millis(context.spinner.interval));
-        });
-
-        Self { sender, handle }
-    }
 }
