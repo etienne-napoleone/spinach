c693988e358140ecc38fef22b9ffeec0b7345cbd
etienne-napoleone <etienne.napoleone@gmail.com>
2022-03-26 18:29:10 +0100

    feat: manage strings as input

---
+++ src/helper.rs
@@ -0,0 +1,36 @@
+pub struct OptText {
+    pub inner: Option<String>,
+}
+
+impl Default for OptText {
+    fn default() -> Self {
+        Self { inner: None }
+    }
+}
+
+impl From<String> for OptText {
+    fn from(string: String) -> Self {
+        Self {
+            inner: Some(string),
+        }
+    }
+}
+
+impl From<&str> for OptText {
+    fn from(string_slice: &str) -> Self {
+        Self {
+            inner: Some(string_slice.to_string()),
+        }
+    }
+}
+
+impl From<Option<&str>> for OptText {
+    fn from(option: Option<&str>) -> Self {
+        match option {
+            Some(string_slice) => Self {
+                inner: Some(string_slice.to_string()),
+            },
+            None => Self::default(),
+        }
+    }
+}
